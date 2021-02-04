17f5bbe69a2656c578c66166d4a56126f7cb9dbc
EdwardReed1 <edward.reed@revature.net>
2021-02-04 14:20:20 -0500

    Added the graphs for the results

--- BD_project/EdwardDir/testQueries.scala
+++ BD_project/EdwardDir/testQueries.scala
@@ -1,9 +1,13 @@
 package review
 
+import java.io.File
+
+import com.cibo.evilplot.plot.{Bar, BarChart}
+import com.cibo.evilplot.plot.aesthetics.DefaultTheme._
 import org.apache.spark
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.{DataFrame, SparkSession, functions}
-import org.apache.spark.sql.functions.{asc, count, desc, explode, max, round, second}
+import org.apache.spark.sql.functions.{asc, concat, count, desc, explode, length, lit, max, round, second}
 
 object testQueries {
   def main(args: Array[String]): Unit = {
@@ -20,8 +24,9 @@ object testQueries {
     //val events = spark.read.option("sep", "\t").option("header", "true").csv(args(0))
     //  .withColumn("rsvp_count", $"yes_rsvp_count".cast("Int"))
 
-    val events = spark.read.parquet(args(0))
+    val events = spark.read.parquet("full_data.parquet")
       .withColumn("rsvp_count", $"yes_rsvp_count".cast("Int"))
+      .withColumn("Venue State", $"localized_location".substr(length($"localized_location") - 2, length($"localized_location") - 1))
 
     //This dataframe just has the city (localized_location) and a column containing how many events were
     //held in the city.
@@ -30,7 +35,7 @@ object testQueries {
       .agg(count("city").as("number of events"))
       .sort(functions.desc("number of events"))
 
-   // cities.show(false)
+    //cities.show(false)
 
     //Writes the answer to question 8 to file.
     cities.coalesce(1).write.option("header", "true").option("delimiter", "\t").csv("q8.tsv")
@@ -40,7 +45,21 @@ object testQueries {
     //city and amount of rsvps received
     val venues = events.select($"name".as("Event Name"), $"v_name".as("Venue Name"),
       $"local_date".as("Date of Event"), $"localized_location".as("City"),
-      $"yes_rsvp_count".as("RSVPs received"), $"group_name".as("Group Name"))
+      $"yes_rsvp_count".as("RSVPs received"), $"group_name".as("Group Name"), $"Venue State")
+
+    val distinctVenues = venues.select($"Venue Name", $"Venue State").distinct()
+
+    //Creates a dataframe that will contain the each state and the number of venues in each state
+    val numStates = distinctVenues.select($"Venue State")
+      .groupBy("Venue State")
+      .agg(count("Venue State").as("Number of Venues"))
+      .orderBy(functions.desc("Number of Events"))
+
+    numStates.coalesce(1).write.option("header", "true").option("delimiter", "\t").csv("venues_per_state.tsv")
+
+    val forGraph = numStates.select($"*").limit(15)
+
+    plot(forGraph, "States with the most venues", "venues_per_state")
 
     //Returns a dataframe that contains the venue name, the group that hosted the event's name and the location
     //  ot the group
@@ -55,40 +74,36 @@ object testQueries {
     //Writes the answer to question 15 to a file.
     popVenues.coalesce(1).write.option("header", "true").option("delimiter", "\t").csv("q15.tsv")
 
-    //Side question which group hosted the most events
-    val popGroups = venues.select($"Group Name", $"City")
-      .groupBy("Group Name", "City")
-      .agg(count("Group Name").as("Number of hosted events"))
-      .orderBy(functions.desc("Number of hosted events"))
-      .limit(10)
-      //.show(false)
+  }
 
-    //These are a few queries that show the events from venues that hosted the most events with the
-    //most rsvps received.
+  def plot(df: DataFrame, title: String, fname: String): Unit = {
+    // Convert dataframe into sequence of doubles for axes
+    val seq = df.collect().map(row => row.toSeq.map(_.toString)).toSeq.flatten
 
-    val nova = venues.select("*")
-      .filter($"Venue Name" === "Nova Labs")
-      .orderBy(functions.desc("RSVPs received"))
-      .limit(10)
-      //.show(false)
+    var counts = Seq[Double]()  // y-axis
+    var labels = Seq[String]()  // x-axis
 
-    val omni = venues.select("*")
-      .filter($"Venue Name" === "Counter Culture Labs, at the Omni Commons")
-      .orderBy(functions.desc("RSVPs received"))
-      .limit(10)
-      //.show(false)
-
-    val corp = venues.select("*")
-      .filter($"Venue Name" === "Extensive Business Solutions Corp.")
-      .orderBy(functions.desc("RSVPs received"))
-      .limit(10)
-      //.show(false)
-
-    val plano = venues.select("*")
-      .filter($"Venue Name" === "TheLab.ms - Plano's Makerspace")
-      .orderBy(functions.desc("RSVPs received"))
-      .limit(10)
-      //.show(false)
-
+    // Populate x-axis and y-axis
+    for(i <- 0 until seq.length){
+      if(i%2 == 0){
+        labels = labels :+ (seq(i))
+      }
+    }
+    for(i <- 0 until seq.length){
+      if(i%2 != 0){
+        counts = counts :+ seq(i).toDouble
+      }
+    }
+    // Plot a bar chart
+    BarChart.custom(counts.map(Bar.apply), spacing = Some(35))
+      .title(title)
+      .standard(xLabels = labels)
+      .xbounds(lower = 0)
+      //.hline(0)
+      //.xAxis(labels)
+      //.yAxis()
+      //.frame()
+      .render()
+      .write(new File(s"C:/Users/kingr/IdeaProjects/staging-project/${fname}.png"))
   }
 }
